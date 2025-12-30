da455919251169a9d5855d195852e6f6141111b5
gajera ayush <ayushgajera50@gmail.com>
2025-12-30 23:56:30 +0530

    feat: add utility to generate JWT and set as http-only cookie

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -2,15 +2,16 @@ import jwt from "jsonwebtoken"
 
 export const generateToken = async (res, user, message) => {
     console.log(user)
-    const token = jwt.sign({ userId: user._id ,role:user.role}, process.env.SECRET_KEY, { expiresIn: "7d" });
+    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.SECRET_KEY, { expiresIn: "7d" });
     res.status(200)
         .cookie("token", token, {
             httpOnly: true,
-            sameSite: 'strict',
+            sameSite: 'None',
+            secure: true,
             maxAge: 24 * 60 * 60 * 1000  //1 day
         }).json({
             success: true,
             message,
-            user 
+            user
         });
 }
